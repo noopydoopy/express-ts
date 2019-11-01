b9fa529f5078e2acd276704af0741d85ca13b80d
sooparerk sakyai <sooparerk@mycostech.com>
2019-11-01 11:15:37 +0700

    export default for index

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,27 @@
 import express from 'express';
 import userRoutes from './routes/user';
 const app = express();
-const port = 8080; // default port to listen
+// const port = 8080; // default port to listen
 
-app.use('/users', userRoutes);
-// define a route handler for the default home page
-app.get('/', (req, res) => {
-    res.send('Hello world!');
-});
+// app.use('/users', userRoutes);
+// // define a route handler for the default home page
+// app.get('/', (req, res) => {
+//     res.send('Hello world!');
+// });
 
-app.get('/today', (req, res) => {
-    res.send(new Date());
-});
+// app.get('/today', (req, res) => {
+//     res.send(new Date());
+// });
 
-app.get('/future/:day', (req, res) => {
-    let date = new Date();
-    date.setDate(date.getDate() + Number(req.params.day));
-    res.send(date);
-})
+// app.get('/future/:day', (req, res) => {
+//     let date = new Date();
+//     date.setDate(date.getDate() + Number(req.params.day));
+//     res.send(date);
+// })
 
-// start the Express server
-app.listen(port, () => {
-    console.log(`server started at http://localhost:${port}`);
-});
+// // start the Express server
+// app.listen(port, () => {
+//     console.log(`server started at http://localhost:${port}`);
+// });
+
+export default userRoutes;
