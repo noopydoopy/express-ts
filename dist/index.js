1e2515a6c1cb9c4cc49ff884ee7392dee07286bf
sooparerk sakyai <sooparerk@mycostech.com>
2019-11-01 11:25:53 +0700

    Update dist

--- dist/index.js
+++ dist/index.js
@@ -6,22 +6,23 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const user_1 = __importDefault(require("./routes/user"));
 const app = express_1.default();
-const port = 8080; // default port to listen
-app.use('/users', user_1.default);
-// define a route handler for the default home page
-app.get('/', (req, res) => {
-    res.send('Hello world!');
-});
-app.get('/today', (req, res) => {
-    res.send(new Date());
-});
-app.get('/future/:day', (req, res) => {
-    let date = new Date();
-    date.setDate(date.getDate() + Number(req.params.day));
-    res.send(date);
-});
-// start the Express server
-app.listen(port, () => {
-    console.log(`server started at http://localhost:${port}`);
-});
+// const port = 8080; // default port to listen
+// app.use('/users', userRoutes);
+// // define a route handler for the default home page
+// app.get('/', (req, res) => {
+//     res.send('Hello world!');
+// });
+// app.get('/today', (req, res) => {
+//     res.send(new Date());
+// });
+// app.get('/future/:day', (req, res) => {
+//     let date = new Date();
+//     date.setDate(date.getDate() + Number(req.params.day));
+//     res.send(date);
+// })
+// // start the Express server
+// app.listen(port, () => {
+//     console.log(`server started at http://localhost:${port}`);
+// });
+exports.default = user_1.default;
 //# sourceMappingURL=index.js.map
